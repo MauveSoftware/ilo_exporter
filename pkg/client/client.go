f71c1761c3405a093f09a0c276b8c8680606ad4a
Daniel Czerwonk <daniel@dan-nrw.de>
2023-02-09 08:46:12 +0100

    add SPDX-License information
    Signed-off-by: Daniel Czerwonk <daniel@dan-nrw.de>

--- pkg/client/client.go
+++ pkg/client/client.go
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: (c) Mauve Mailorder Software GmbH & Co. KG, 2020. Licensed under [MIT](LICENSE) license.
+//
+// SPDX-License-Identifier: MIT
+
 package client
 
 type Client interface {
